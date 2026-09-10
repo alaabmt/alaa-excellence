f8182f7ac8d3361fdb97ba84ea80baddfadc713e
alaabmt <160816140+alaabmt@users.noreply.github.com>
2026-09-10 20:15:56 +0400

    Wire protected assessment endpoints

--- assets/js/auth-config.js
+++ assets/js/auth-config.js
@@ -1,6 +1,8 @@
 window.TAMAYUZ_AUTH_CONFIG = Object.freeze({
   provider: 'supabase',
   publicConfigEndpoint: 'https://dqrsjhcfmpjhcxvtxosy.supabase.co/functions/v1/public-auth-config',
+  protectedContentEndpoint: 'https://dqrsjhcfmpjhcxvtxosy.supabase.co/functions/v1/assessment-content',
+  attemptsEndpoint: 'https://dqrsjhcfmpjhcxvtxosy.supabase.co/functions/v1/assessment-attempts',
   loginPath: '/account/login.html',
   registerPath: '/account/register.html',
   accountPath: '/account/',
