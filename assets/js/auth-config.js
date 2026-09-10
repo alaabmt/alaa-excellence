f869ea9519284b19074d3d47d4d5ddfeee029bf8
alaabmt <160816140+alaabmt@users.noreply.github.com>
2026-09-10 19:22:34 +0400

    Add assessment authentication configuration scaffold

---
+++ assets/js/auth-config.js
@@ -0,0 +1,12 @@
+window.TAMAYUZ_AUTH_CONFIG = Object.freeze({
+  provider: 'supabase',
+  supabaseUrl: '',
+  supabaseAnonKey: '',
+  loginPath: '/account/login.html',
+  registerPath: '/account/register.html',
+  accountPath: '/account/',
+  protectedPrefixes: [
+    '/tools/learning-preference-profile/',
+    '/tools/work-approach-assessment/'
+  ]
+});
