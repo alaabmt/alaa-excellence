50245504919387d2306ab2ed169581a050653ab4
alaabmt <160816140+alaabmt@users.noreply.github.com>
2026-09-10 20:35:08 +0400

    Document optional report email endpoint

--- assets/js/auth-config.js
+++ assets/js/auth-config.js
@@ -2,7 +2,7 @@ window.TAMAYUZ_AUTH_CONFIG = Object.freeze({
   provider: 'supabase',
   publicConfigEndpoint: 'https://dqrsjhcfmpjhcxvtxosy.supabase.co/functions/v1/public-auth-config',
   protectedContentEndpoint: 'https://dqrsjhcfmpjhcxvtxosy.supabase.co/functions/v1/assessment-content',
-  attemptsEndpoint: 'https://dqrsjhcfmpjhcxvtxosy.supabase.co/functions/v1/assessment-attempts',
+  reportEmailEndpoint: '',
   loginPath: '/account/login.html',
   registerPath: '/account/register.html',
   accountPath: '/account/',
